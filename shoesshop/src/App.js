a7dc5c1123b01623184c5afa46d059c3f599a8eb
Celina Takeda <takedacarina@gmail.com>
2022-03-25 10:07:45 -0300

    commit

--- shoesshop/src/App.js
+++ shoesshop/src/App.js
@@ -1,58 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './components/store';
+import Routes from './routes';
+import { Container } from '@material-ui/core/';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+const App = () => {
+  
+  const localCart = JSON.parse(localStorage.getItem('dioshopping: cart'))
+  
+  if(localCart !== null) {
+    store.dispatch({type: 'CHANGE_CART', localCart})
+  }
+  
+  return(
+    <Provider store={store}>
+      <Container maxWidth="xl">
+        <Router>
+          <Header />
+          <Routes />
+        </Router>
+      </Container> 
+    </Provider>
+  )
 }
 
 export default App;
+
+
